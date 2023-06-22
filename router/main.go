b481a99832756cb4d76052cef0d0a12930edeeb5
Aditya Arun Rudrawar <arudrawa@iu.edu>
2023-06-21 18:23:37 -0700

    Add routes

--- router/main.go
+++ router/main.go
@@ -4,6 +4,19 @@ import (
 	"github.com/adityarudrawar/go-backend/handlers"
 	"github.com/gofiber/fiber/v2"
 )
+
+
 func SetupRoutes(app *fiber.App) {
+	// Base: Ping
 	app.Get("/health", handlers.HandleHealthCheck)
+
+	auth := app.Group("/register")
+	auth.Post("/signup", handlers.HandleSignup)
+	auth.Post("/login", handlers.HandleLogin)
+
+	message := app.Group("/message")
+	message.Get("/messages", handlers.HandleHealthCheck)
+	message.Post("/send_message", handlers.HandleHealthCheck)
+	message.Post("/upvote/:id", handlers.HandleHealthCheck)
+	message.Post("/downvote/:id", handlers.HandleHealthCheck)
 }
