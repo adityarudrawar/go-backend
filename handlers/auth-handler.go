58d458fdc1b8b7a609c25c861ae07166a3333d6d
adityarudrawar <adirudrawar17@gmail.com>
2023-06-27 16:50:32 -0400

    Refactore authentication handlers
    -- Refactor Register handler to handle different usecases
    -- Make the SecretKey global

--- handlers/auth-handler.go
+++ handlers/auth-handler.go
@@ -11,7 +11,8 @@ import (
 )
 
 // TODO: Write better response objects for each error
-// TODO: Think of better test cases and cover them in code.
+
+var SecretKey = "dhkjgbfkljkljbdlkjbfjkb"
 
 func HandleRegister(c *fiber.Ctx) error {
 	
@@ -27,18 +28,30 @@ func HandleRegister(c *fiber.Ctx) error {
 		Password: password,
 	}
 	
-	// TODO: When the username already exists, dont increment the id counter and send the error message
-	if dbc := database.DB.Create(&user); dbc.Error != nil {
-		message := "Dublicate user name"
-		return c.JSON(message)
+	u := database.DB.FirstOrCreate(&user)
+
+	if u.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(
+			fiber.Map{
+				"error": "Internal server error",
+			})
+	}
+
+	if u.RowsAffected == 1 {
+		return c.Status(fiber.StatusCreated).JSON(
+			fiber.Map{
+				"message": "User created successfully",
+			})
 	}
 	
-	return c.JSON(user)
+	return c.Status(fiber.StatusBadRequest).JSON(
+		fiber.Map{
+			"error": "Username already exists",
+		})
 }
 
 
 func HandleLogin(c *fiber.Ctx) error {
-	// TODO: If the user is already logged in, send messages
 	var data map[string]string
 
 	if err := c.BodyParser(&data); err != nil {
@@ -47,15 +60,16 @@ func HandleLogin(c *fiber.Ctx) error {
 
 	var user models.User
 
-	database.DB.Where("username = ?", data["username"]).First(&user)
+	dbc := database.DB.Where("username = ?", data["username"]).First(&user)
 
-	if user.Id == 0 {
+	if dbc.RowsAffected == 0 {
 		c.Status(fiber.StatusNotFound)
 		return c.JSON(fiber.Map{
 			"message": "User not found",
 		})
 	}
 
+	// Compare the hash of the password
 	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON(fiber.Map{
@@ -67,8 +81,6 @@ func HandleLogin(c *fiber.Ctx) error {
 		Issuer:    user.Username,
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1 day
 	})
-	
-	SecretKey := "dhkjgbfkljkljbdlkjbfjkb"
 	
 	token, err := claims.SignedString([]byte(SecretKey))
 
@@ -96,8 +108,6 @@ func HandleLogin(c *fiber.Ctx) error {
 func HandleGetUser(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 	
-	SecretKey := "dhkjgbfkljkljbdlkjbfjkb"
-
 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(SecretKey), nil
 	})
